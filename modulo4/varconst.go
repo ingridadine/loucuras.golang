86ef7c9908cba9c9c1e55fbd4bccd57dea7a7ac2
ingridadine <66760636+ingridadine@users.noreply.github.com>
2024-10-26 20:24:35 -0300

    Test some examples for variables
    Inferência de tipo :=

--- modulo4/varconst.go
+++ modulo4/varconst.go
@@ -8,6 +8,23 @@ func main() {
 	nome = "Ingrid"
 	fmt.Println(nome)
 
-	nome = "Ingrid"
+	nome = "Adine"
 	fmt.Println(nome)
+
+	var idade int
+	idade = 35
+	fmt.Println(idade)
+
+	var namae = "Dinyxia"
+	fmt.Println("Watashi no namae wa", namae, "desu.")
+
+	var peso, salario int = 65, 5000
+	fmt.Println(peso)
+	fmt.Println(salario)
+
+	var employed = true
+	fmt.Println(employed)
+
+	nacionalidade := "brasileira"
+	fmt.Println(nacionalidade)
 }
