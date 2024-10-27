73ee6908430dcee178d77d2ab618e1140ec36429
ingridadine <66760636+ingridadine@users.noreply.github.com>
2024-10-26 21:31:00 -0300

    Create zerovalues.go

---
+++ modulo4/zerovalues.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i int
+	fmt.Printf("Inteiro: %v\n", i)
+
+	var f float64
+	fmt.Printf("Float: %v\n", f)
+
+	var b bool
+	fmt.Printf("Bool: %v\n", b)
+
+	var s string
+	fmt.Printf("String: %q\n", s)
+}
