99f93631740d322f9aba155ea6941959a6ea6120
ingridadine <66760636+ingridadine@users.noreply.github.com>
2024-10-30 22:20:28 -0300

    Add var string example

--- modulo4/funcoes.go
+++ modulo4/funcoes.go
@@ -6,14 +6,21 @@ func main() {
 	// somaDosValores := soma(42, 13)
 	// fmt.Println(somaDosValores)
 
-	subtraiValores := subtracao(42, 13)
-	fmt.Println(subtraiValores)
+	// subtraiValores := subtracao(42, 13)
+	// fmt.Println(subtraiValores)
+
+	babado := revelaBabado("ui ui ui")
+	fmt.Println(babado)
+}
+
+func revelaBabado(babado string) string {
+	return babado
 }
 
 // func soma(x int, y int) int {
 // 	return x + y
 // }
 
-func subtracao(x int, y int) int {
-	return x - y
-}
+// func subtracao(x int, y int) int {
+// 	return x - y
+// }
