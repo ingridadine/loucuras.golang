e64a7c429ceb71ca9f690422b786192372eed3b2
ingridadine <66760636+ingridadine@users.noreply.github.com>
2024-10-30 22:07:58 -0300

    Add subtraction operation

--- modulo4/funcoes.go
+++ modulo4/funcoes.go
@@ -3,10 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	somaDosValores := soma(42, 13)
-	fmt.Println(somaDosValores)
+	// somaDosValores := soma(42, 13)
+	// fmt.Println(somaDosValores)
+
+	subtraiValores := subtracao(42, 13)
+	fmt.Println(subtraiValores)
 }
 
-func soma(x int, y int) int {
-	return x + y
+// func soma(x int, y int) int {
+// 	return x + y
+// }
+
+func subtracao(x int, y int) int {
+	return x - y
 }
