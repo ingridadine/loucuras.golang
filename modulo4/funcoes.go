fe33efdc021a94c72aa47d745355001ce7300ec2
ingridadine <66760636+ingridadine@users.noreply.github.com>
2024-11-10 09:12:37 -0300

    Add "_" in funcoes.go
    Adicionando mais variáveis in line e utilizando o recurso blank identifier. It avoids having to declare all the variables for the returns values.

--- modulo4/funcoes.go
+++ modulo4/funcoes.go
@@ -9,12 +9,14 @@ func main() {
 	// subtraiValores := subtracao(42, 13)
 	// fmt.Println(subtraiValores)
 
-	babado := revelaBabado("ui ui ui")
-	fmt.Println(babado)
+	babado1, babado2, _ := revelaBabado("ui ui ui") //O "_" serve para omitir o print da variável babado3
+	fmt.Println(babado1)
+	fmt.Println(babado2)
+	//fmt.Println(babado3)
 }
 
-func revelaBabado(babado string) string {
-	return babado
+func revelaBabado(babado string) (string, string, string) {
+	return babado, babado, babado
 }
 
 // func soma(x int, y int) int {
